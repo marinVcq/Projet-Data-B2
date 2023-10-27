99925034be10ec5858a5d91ad1b2822207c103cf
marinVcq <marinvacquier@hotmail.fr>
2023-10-27 10:57:52 +0200

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Other webpack configuration options...
+  
+    devServer: {
+      // Other devServer options...
+  
+      // Add the following line to disable host checking
+      disableHostCheck: true,
+    },
+  };
