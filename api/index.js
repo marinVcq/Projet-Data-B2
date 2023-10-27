8a57b2f4f242626bd31cd1c2351c00272b4f88e8
marinVcq <marinvacquier@hotmail.fr>
2023-10-27 13:57:56 +0200

    test

--- api/index.js
+++ api/index.js
@@ -1,23 +1,38 @@
-import express from "express"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cookieParser from "cookie-parser";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 // import routes
-import laboRoutes from "./routes/labos.js"
-import authRoutes from "./routes/auth.js"
-import employeeRoutes from "./routes/employees.js"
+import laboRoutes from "./routes/labos.js";
+import authRoutes from "./routes/auth.js";
+import employeeRoutes from "./routes/employees.js";
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+// Add middleware to set up CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 app.use(cookieParser());
-app.use("/api/labos",laboRoutes );
+app.use("/api/labos", laboRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
 
+// Add the proxy middleware for '/api' requests
+app.use("/api", createProxyMiddleware({
+target: 'https://fortunato-api.onrender.com',
+changeOrigin: true,
+}));
+
 
 app.listen(process.env.DB_PORT, () => {
-    console.log("connected");
+  console.log("connected");
 });
--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -8,14 +8,28 @@ export const AuthContextProvider = ({ children }) => {
     JSON.parse(localStorage.getItem("user")) || null
   );
 
+  const api = axios.create({
+    baseURL: "/api", // Relative URL for the proxy
+  });
+
   const login = async (inputs) => {
-    const res = await axios.post("https://fortunato-api.onrender.com/auth/login", inputs);
-    setCurrentUser(res.data);
+    try {
+      const res = await api.post("https://fortunato-api.onrender.com/auth/login", inputs);
+      setCurrentUser(res.data);
+    } catch (error) {
+      // Handle login error
+      console.error("Login failed", error);
+    }
   };
 
-  const logout = async (inputs) => {
-    await axios.post("/auth/logout");
-    setCurrentUser(null);
+  const logout = async () => {
+    try {
+      await api.post("https://fortunato-api.onrender.com/auth/logout");
+      setCurrentUser(null);
+    } catch (error) {
+      // Handle logout error
+      console.error("Logout failed", error);
+    }
   };
 
   useEffect(() => {
