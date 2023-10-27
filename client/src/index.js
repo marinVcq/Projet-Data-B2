6fbd5567faf69c735556661579b9d57613622122
marinVcq <marinvacquier@hotmail.fr>
2023-10-27 09:05:52 +0200

    fix test

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from "./context/authContext";
 
+// Import styles
+import "./style/main.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
